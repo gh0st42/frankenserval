367b040f6f56ccc6c94e2f62664c72dc4d0074de
lbaumgaertner <lbaumgaertner@mathematik.uni-marburg.de>
2018-09-04 14:20:00 +0200

    updated readme and initial python source

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # frankenserval
 Frankenstein's Serval 
+```
+
+                                                   .....
+                                                  C C  /
+                                                 /<   /
+                                  ___ __________/_#__=o
+                                /(- /(\_\________   \
+                                \ ) \ )_      \o     \
+                                /|\ /|\       |'     |
+                                              |     _|
+                                              /o   __\
+                                             / '     |
+                                            / /      |
+                                           /_/\______|
+    _                ___       _.--.      (   _(    <
+    \`.|\..----...-'`   `-._.-'_.-'`       \    \    \
+    /  ' `         ,       __.--'           \    \    |
+    )/' _/     \   `-_,   /                  \____\____\
+    `-'" `"\_  ,_.-;_.-\_ ',     fsc/as    ____\_\__\_\
+        _.-'_./   {_.'   ; /              /`   /`     o\
+       {_.-``-'         {_/               |___ |_______|.. . b'ger
+```
+
+
+
+    
+
+    
+
+
+               
+
+Proof-of-Concept of a MDP-less serval communicating only over HTTP similar to (forban)[https://github.com/adulau/Forban]. This is a hacky python version, current rhizome state is locally checked via restful every 2 seconds. HTTP direct sync is only triggered if two nodes announce different hashes. 
+
+## Usage
+
+Prevent serval from using MDP on any interface by removing them from the config:
+```
+$ servald config del interfaces.0.match
+```
+
+Make sure that pum:pum123 have restful access to serval instance on 127.0.0.1:
+```
+$ servald config set api.restful.users.pum.password pum123
+```
+
+Finally, execute `python frankenserval.py`
+
+*NOTE: Default announcement interval 2s*
---
+++ frankenserval.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+
+# Frankenstein's Serval
+#
+# Copyright: Lars Baumgaertner (c) 2018
+
+
+import socket
+import struct
+import threading
+import time
+import sys
+import platform
+import fcntl
+import urllib2
+import base64
+import json
+import hashlib
+import subprocess
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+
+SERVALPORT = 4110
+ANNOUNCEMENT_INTERVAL = 2
+USERNAME = "pum"
+PASSWORD = "pum123"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind((MCAST_GRP, MCAST_PORT))
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+ 
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+please_run = True
+
+myself_data = ["FrankenServal", platform.node(), get_ip_address("eth0"), "%s" % SERVALPORT]
+mystate = ""
+
+def get_my_bundlelist():
+    request = urllib2.Request("http://127.0.0.1:4110/restful/rhizome/bundlelist.json")
+    base64string = base64.b64encode('%s:%s' % (USERNAME, PASSWORD))
+    request.add_header("Authorization", "Basic %s" % base64string)
+    result = urllib2.urlopen(request)
+    return json.loads(result.read())
+
+def get_bundle_state():
+    id_field_idx = 3
+    m = hashlib.md5()
+    bundles = get_my_bundlelist()
+    bids = []
+    for i in bundles['rows']:
+        bids.append(i[id_field_idx])
+
+    for i in sorted(bids):
+        m.update(i)
+
+    return m.hexdigest()
+
+def announce_thread():
+    global please_run
+    global myself_data
+    global mystate
+
+    MCAST_GRP = '224.1.1.1'
+    MCAST_PORT = 5007
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+    try:
+        while please_run:
+            #print get_bundle_state()    
+            #print "announcing ", platform.node(), get_ip_address('eth0')
+            mystate = get_bundle_state()
+            sock.sendto(" ".join(myself_data)+ " " + mystate, (MCAST_GRP, MCAST_PORT))
+            time.sleep(ANNOUNCEMENT_INTERVAL)
+    except Exception as e:
+        print "ending announce thread ", e
+        please_run = False
+        sys.exit(1)
+
+thread = threading.Thread(target=announce_thread, args=())
+try:
+    thread.start()
+except (KeyboardInterrupt, SystemExit):
+    sys.exit()
+
+try:
+    while please_run:
+        received = sock.recv(1024)
+        if received.startswith("FrankenServal"):
+            fields = received.split(" ")
+            if len(fields) == 5 and fields[0:4] != myself_data:
+                node_state = fields[4]
+                if mystate == node_state:
+                    #print "in sync"
+                    pass
+                else:
+                    print "ANNOUNCE: %s %s %s %s - sync required" % (fields[1], fields[2], fields[3], fields[4])
+                    subprocess.call("servald rhizome direct sync http://%s:%s" % (fields[2], fields[3]), shell=True)
+except:
+    print "ending receive thread "
+    please_run = False
+    sys.exit(1)
